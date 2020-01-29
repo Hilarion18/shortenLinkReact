81435bf945aac25f870ef07700158042033bdecb
hilarion_fore <hilarion@fore.coffee>
2020-01-29 18:50:21 +0700

    update didmount data

--- short-link/src/components/home/HomeComponent.js
+++ short-link/src/components/home/HomeComponent.js
@@ -25,22 +25,57 @@ class HomeComponent extends Component {
       }
     })
       .then((value) => {
-        this.links = value.data.data
-        console.log(`this.links`, this.links)
+        this.setState({
+          links: value.data.data
+        })
       })
       .catch((err) => {
         alert(err.message)
       })
   }
 
-
-
-  handleSubmit = () => {
-
+  handleAddLink = () => {
+    axios({
+      method: 'POST',
+      url: `${config.port}/link`,
+      data: this.state.link,
+      headers: {
+        token: localStorage.getItem("token")
+      }
+    })
+      .then((result) => {
+        // if (this.isLogin) {
+        //   this.getLinkData()
+        // } else {
+          this.getLinkData()
+          this.links.push(result.data.data)
+        // }
+      })
+      .catch((err) => {
+        // alert(err.message)
+        alert('there is something wrong, please try again later')
+      })
   }
 
   removeAllLinks = () => {
-
+    if (this.isLogin) {
+      axios({
+        method: `DELETE`,
+        url: `${config.port}/link`,
+        headers: {
+          // id: localStorage.get('userId'),
+          // token: localStorage.getItem('token')
+        }
+      })
+        .then((value) => {
+          // this.links = []
+        })
+        .catch((err) => {
+          alert(err.message)
+        })
+    } else {
+      this.state.links = []
+    }
   }
 
   renderListItem = () => {
@@ -48,15 +83,46 @@ class HomeComponent extends Component {
       <ul>
         {
           this.state.links.map((val, index) => 
-            // return (
-              <li key={index}>
-                { val.shortUrl }
-              </li>
-            // );
+          <tr>
+            <td className="text-left td">
+              <div className="row">
+                <input type="hidden" id="get-link" value={val.shortUrl}/>
+                <a className="copy-link" href="" onClick={this.copyToClipboard}>{val.shortUrl}</a>
+                <div className="text-cursor">click to copy this link</div>
+              </div>
+              {
+                (50 < val.longUrl.length)
+                ? <a>{val.longUrl}</a>
+                : <a>{ val.longUrl.substring(0,50)+"..." }</a>
+              }
+            </td>
+            <td className="visit">will be update soon</td>
+            <td className="last-visited">will be update soon</td>
+          </tr>
           )
         }
       </ul>
     )
+  }
+
+  copyToClipboard() {
+    let testingCodeToCopy = document.querySelector('#get-link')
+    testingCodeToCopy.setAttribute('type', 'text')    // 不是 hidden 才能複製
+    testingCodeToCopy.select()
+    let item = document.getElementById('get-link').value
+    console.log(`item`, item)
+
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      alert(`your url ${item} has been copied ` + msg);
+    } catch (err) {
+      alert('Oops, unable to copy');
+    }
+
+    /* unselect the range */
+    testingCodeToCopy.setAttribute('type', 'hidden')
+    window.getSelection().removeAllRanges()
   }
 
   render() {
@@ -73,14 +139,14 @@ class HomeComponent extends Component {
             />
           </div>
           <div className="col col-button">
-            <button className="button button--winona button--border-thin button--round-s" data-text="generate link" onClick={this.handleSubmit}><span>generate link</span></button>
+            <button className="button button--winona button--border-thin button--round-s" data-text="generate link" onClick={this.handleAddLink}><span>generate link</span></button>
           </div>
         </div>
         {/* <header className="App-header"> */}
           <div className="container home-content">
             <div className="row">
               <h5 className="text-left">Previously shortened by you</h5>
-              <a href="" className="clear-history" onClick={this.removeAllLinks}>Clear history</a>
+              {/* <a href="" className="clear-history" onClick={this.removeAllLinks}>Clear history</a> */}
             </div>
             <table className="item-table">
               <thead>
@@ -90,27 +156,6 @@ class HomeComponent extends Component {
               </thead>
               <tbody  >
                 {this.renderListItem()}
-                {/* { (this.state.links && 0 < this.state.links.length)
-                  ? this.state.links.map((item, index) => {
-                    return (
-                      <h1>{item.shortUrl}</h1>
-                    )}
-                  )
-                  : null
-                } */}
-                {/* <tr :key="index" v-for="(link , index) in links">
-                  <td className="text-left td">
-                    <div className="row">
-                      <input type="hidden" id="get-link" :value="link.shortUrl">
-                      <a className="copy-link" href="" @click.stop.prevent="copyToClipboard">{{link.shortUrl}}</a>
-                      <div className="text-cursor">click to copy this link</div>
-                    </div>
-                    <a className="long-url" v-if="link.longUrl.length<50">{{link.longUrl}}</a>
-                    <a v-else>{{ link.longUrl.substring(0,50)+"..." }}</a>
-                  </td>
-                  <td className="visit">will be update soon</td>
-                  <td className="last-visited">will be update soon</td>
-                </tr> */}
               </tbody>
             </table>
           </div>
