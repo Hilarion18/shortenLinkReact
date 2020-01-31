ddcde3cc71f4542fb6d83d42cae37fe6dddfe2c1
hilarion_fore <hilarion@fore.coffee>
2020-02-01 01:57:28 +0700

    clean up code server

--- server/src/helpers/hashLink.js
+++ server/src/helpers/hashLink.js
@@ -1,20 +1,14 @@
 require('dotenv').config()
 import BitlyAPI from "node-bitlyapi"
 
+// login our access to bit.ly API
 var Bitly = new BitlyAPI({
 	client_id: process.env.BITLY_ID,
 	client_secret: process.env.BITLY_SECRET
 });
+
+// getting token to access to bit.ly API
 const bitly_token = [process.env.BITLY_TOKEN]
 Bitly.setAccessToken(bitly_token);
-// 	$login="urlogin";
-// 	$appkey="ur_api_key";   
-
-// 	$bitly = 'http://api.bit.ly/shorten?version='.$version.'&longUrl='.urlencode($url).'&login='.$login.'&apiKey='.$appkey.'&format='.$format;
-// 			$response = file_get_contents($bitly);
-
-// 			$xml = simplexml_load_string($response);
-
-// 	return $response;
 
 export default Bitly
--- server/src/routes/link.js
+++ server/src/routes/link.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { getLinks, createLink, deleteAll } from '../controllers/link'
 const router = express.Router();
 
-/* GET users listing. */
+/* Link routing. */
 router.get('/', getLinks);
 router.post('/', createLink);
 router.delete('/', deleteAll)
